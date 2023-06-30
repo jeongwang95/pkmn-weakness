0e441422e702954aa8bf29b8a999a15e4f869854
Jeong Wang <jeong@jeongs-mbp.myfiosgateway.com>
2023-06-30 12:37:23 -0400

    added damage relations

--- main.js
+++ main.js
@@ -8,8 +8,47 @@ const getPkmn = async (name) => {
     }
 };
 
+// retrieve type damage relations from PokeAPI
+const getDmg = async (type) => {
+    try {
+        let response = await axios.get(`https://pokeapi.co/api/v2/type/${type}`);
+        let data = response.data;
+        return data.damage_relations;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+// Gather damage relations for each type then combine it as a string
+const dmgRelations = async (types) => {
+    let s = `${types[0].type.name}:<br>`;
+    const dmgData = await getDmg(types[0].type.name);
+    for (const k in dmgData) {
+        if (k == 'double_damage_from' || k == 'half_damage_from' || k == 'no_damage_from') {
+            s += `${k}: `;
+            dmgData[k].forEach((val) => s += `${val.name} `);
+            s += '<br>';
+        }
+    }
+
+    // if the pokemon has a second type, add the second damage relations to the bottom of the first damage relations
+    if (types.length > 1) {
+        s += `<br>${types[1].type.name}:<br>`;
+        const dmgData2 = await getDmg(types[1].type.name);
+        for (const k in dmgData2) {
+                if (k == 'double_damage_from' || k == 'half_damage_from' || k == 'no_damage_from') {
+                s += `${k}: `;
+                dmgData2[k].forEach((val) => s += `${val.name} `);
+                s += '<br>';
+            }
+        }
+    } 
+
+    return s;
+}
+
 // show pokemon data
-const pkmnData = (image, name, types) => {
+const pkmnData = (image, name, types, dmgRelations) => {
     const html = `
                     <img src="${image}" alt="${name} image">
                     <p>Pokemon: ${name}</p>
@@ -20,6 +59,8 @@ const pkmnData = (image, name, types) => {
                             return `${types[0].type.name}`
                         }
                     })()}</p>
+                    <p>Damage Relations:</p>
+                    <p>${dmgRelations}</p>
                 `;
 
     document.querySelector('.pkmn-data').insertAdjacentHTML('beforeend', html);
@@ -36,5 +77,6 @@ form.addEventListener('submit', async (event) => {
     let pkmn = document.querySelector('#pkmn').value;
 
     const data = await getPkmn(pkmn.toLowerCase());
-    pkmnData(data['sprites']['other']['official-artwork']['front_default'], data.name, data.types);
+    const dmgData = await dmgRelations(data.types);
+    pkmnData(data['sprites']['other']['official-artwork']['front_default'], data.name, data.types, dmgData);
 });
