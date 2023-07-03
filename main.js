a8a8daff2b6cfb2914d0a9bd9dd7671e3ccaa42b
Jeong Wang <jeong@jeongs-mbp.myfiosgateway.com>
2023-07-03 14:34:05 -0400

    revising damage relations for pokemon with two types

--- main.js
+++ main.js
@@ -19,33 +19,54 @@ const getDmg = async (type) => {
     }
 }
 
-// Gather damage relations for each type then combine it as a string
+// Gather damage relations for pokemon that only has a single type then convert data to a string
 const dmgRelations = async (types) => {
-    let s = `${types[0].type.name}:<br>`;
+    let s = "";
     const dmgData = await getDmg(types[0].type.name);
     for (const k in dmgData) {
         if (k == 'double_damage_from' || k == 'half_damage_from' || k == 'no_damage_from') {
             s += `${k}: `;
-            dmgData[k].forEach((val) => s += `${val.name} `);
+            dmgData[k].forEach((type) => s += `${type.name} `);
             s += '<br>';
         }
     }
-
-    // if the pokemon has a second type, add the second damage relations to the bottom of the first damage relations
-    if (types.length > 1) {
-        s += `<br>${types[1].type.name}:<br>`;
-        const dmgData2 = await getDmg(types[1].type.name);
-        for (const k in dmgData2) {
-                if (k == 'double_damage_from' || k == 'half_damage_from' || k == 'no_damage_from') {
-                s += `${k}: `;
-                dmgData2[k].forEach((val) => s += `${val.name} `);
-                s += '<br>';
-            }
-        }
-    } 
-
     return s;
 }
+
+const dmgRelations2 = async (types) => {
+
+}
+
+// Gather damage relations for each type then combine it as a string
+// const dmgRelations = async (types) => {
+//     let s = `${types[0].type.name}:<br>`;
+//     const dmgData = await getDmg(types[0].type.name);
+//     for (const k in dmgData) {
+//         if (k == 'double_damage_from' || k == 'half_damage_from' || k == 'no_damage_from') {
+//             s += `${k}: `;
+//             dmgData[k].forEach((val) => s += `${val.name} `);
+//             s += '<br>';
+//         }
+//     }
+
+//     // if the pokemon has a second type, add the second damage relations to the bottom of the first damage relations
+//     if (types.length > 1) {
+//         s += `<br>${types[1].type.name}:<br>`;
+//         const dmgData2 = await getDmg(types[1].type.name);
+//         for (const k in dmgData2) {
+//                 if (k == 'double_damage_from' || k == 'half_damage_from' || k == 'no_damage_from') {
+//                 s += `${k}: `;
+//                 dmgData2[k].forEach((val) => s += `${val.name} `);
+//                 s += '<br>';
+//             }
+//         }
+
+//         console.log(dmgData);
+//         console.log(dmgData2);
+//     } 
+
+//     return s;
+// }
 
 // show pokemon data
 const pkmnData = (image, name, types, dmgRelations) => {
@@ -77,6 +98,12 @@ form.addEventListener('submit', async (event) => {
     let pkmn = document.querySelector('#pkmn').value;
 
     const data = await getPkmn(pkmn.toLowerCase());
-    const dmgData = await dmgRelations(data.types);
+    let dmgData;
+    if (data.types.length > 1) {
+        dmgData = await dmgRelations2(data.types);
+    } else {
+        dmgData = await dmgRelations(data.types);
+    }
+    
     pkmnData(data['sprites']['other']['official-artwork']['front_default'], data.name, data.types, dmgData);
 });
